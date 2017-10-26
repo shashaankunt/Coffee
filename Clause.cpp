5e980682aa01a8eb608d9b6b734a0600df82cd6b
anuragbandla <32075622+anuragbandla@users.noreply.github.com>
2017-10-26 10:14:25 -0500

    Update Clause.cpp

--- Clause.cpp
+++ Clause.cpp
@@ -11,7 +11,7 @@ public:
   int * xs;
   Clause( int len, int *hgs, int base, int neck,int *xs)
   {
-    this->hgs=hgs; // head+goals pointing to cells in cs
+    this->hgs=hgs; // head+goals pointing to cells in c
     this->base=base; // heap where this starts
     this->len=len; // length of heap slice
     this->neck=neck; // first after the end of the head
